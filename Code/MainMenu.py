70d1675a78c606fb20132bd5a6ac017c8624ee27
NicoSchrodt <nico.schrodt@gmx.de>
2021-12-29 14:05:18 +0100

    Added initial code structure; Main Menu; Abstract Processor

---
+++ Code/MainMenu.py
@@ -0,0 +1,24 @@
+import os.path
+
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.uic import loadUi
+
+
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super(MainMenu, self).__init__()
+        self.init_ui("ui/MainMenu.ui")
+
+    def init_ui(self, ui_name):
+        base_path = os.path.abspath(".")
+        full_path = os.path.join(base_path, ui_name)
+        loadUi(full_path, self)
+
+    def load_Intel8080(self):
+        pass
+
+    def load_Tutorial(self):
+        pass
+
+    def exit_program(self):
+        pass
