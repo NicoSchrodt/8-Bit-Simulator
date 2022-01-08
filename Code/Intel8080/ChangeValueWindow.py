eac3a29cc76428c5065c0506fa57fd3e4484d9fc
NicoSchrodt <nico.schrodt@gmx.de>
2022-01-08 16:07:46 +0100

    Partially implemented registers; Added ChangeWindow for Registers

---
+++ Code/Intel8080/ChangeValueWindow.py
@@ -0,0 +1,40 @@
+import os
+
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtGui import QCloseEvent
+from PyQt6.uic import loadUi
+
+
+class ChangeValueWindow(QMainWindow):
+
+    def __init__(self, Intel8080_MainWindow, btn):
+        super(ChangeValueWindow, self).__init__(Intel8080_MainWindow)
+
+        self.Intel8080_MainWindow = Intel8080_MainWindow
+        self.btn = btn
+
+        self.init_ui("ui\\ChangeValueWindow.ui")
+
+        # Button definition
+        button_ok = self.button_ok
+        button_ok.clicked.connect(self.check_input)
+
+        button_cancel = self.button_cancel
+        button_cancel.clicked.connect(self.close)
+
+    def init_ui(self, ui_name):
+        base_path = os.path.abspath("..")
+        full_path = os.path.join(base_path, ui_name)
+        loadUi(full_path, self)
+
+    def check_input(self):
+        try:
+            value = int(self.lineEdit_value.text(), 16)
+            if 0 <= value < 256:  # May need to be different, depending on register
+                index = self.btn.parent().parent().indexAt(self.btn.pos())
+                self.btn.parent().parent().cellWidget(index.row(), index.column()).setText(self.lineEdit_value.text())
+                if self.btn.parent().parent() == self.Intel8080_MainWindow.Registers_table:
+                    self.Intel8080_MainWindow.update_registers_table()
+                    self.close()
+        except Exception:
+            pass
--- Code/Intel8080/Intel8080_MainWindow.py
+++ Code/Intel8080/Intel8080_MainWindow.py
@@ -1,25 +1,45 @@
 import os
 
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QTableWidgetItem
 from PyQt6.uic import loadUi
 from PyQt6.QtGui import QCloseEvent
 
 from Code.Intel8080.Intel8080 import Intel8080
+from Code.Intel8080.ChangeValueWindow import ChangeValueWindow
 
 
 class Intel8080_MainWindow(QMainWindow):
     def __init__(self, parent):
         super(Intel8080_MainWindow, self).__init__(parent)
         self.init_ui("ui\\Intel8080_MainWindow.ui")
+        self.init_register_table()
         processor = Intel8080()
-        processor.run()
+        #processor.run()
 
     def init_ui(self, ui_name):
         base_path = os.path.abspath("..")
         full_path = os.path.join(base_path, ui_name)
         loadUi(full_path, self)
 
+    def init_register_table(self):
+        for row in range(self.Registers_table.rowCount()):
+            print(row)
+            btn = QPushButton(self.Registers_table)
+            btn.setText('{:x}'.format(0))
+            self.Registers_table.setCellWidget(row, 0, btn)
+            btn.pressed.connect(self.pressed_table_cell)
+
     def closeEvent(self, a0: QCloseEvent):
+        print("Test")
         self.close()
         self.parent().show()
         # Closes Window and Un-Hides MainMenu
+
+    def pressed_table_cell(self):
+        btn = self.sender()
+        self.dialog = ChangeValueWindow(self, btn)
+        self.dialog.show()
+
+    def update_registers_table(self):
+        # To be implemented
+        pass
