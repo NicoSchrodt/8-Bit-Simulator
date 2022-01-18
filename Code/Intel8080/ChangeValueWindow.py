8ba0d6ed4b5254b914fb9799942269f706c13b3c
NicoSchrodt <nico.schrodt@gmx.de>
2022-01-18 14:52:36 +0100

    Don't update interface until valid input received

--- Code/Intel8080/ChangeValueWindow.py
+++ Code/Intel8080/ChangeValueWindow.py
@@ -32,14 +32,17 @@ class ChangeValueWindow(QMainWindow):
         try:
             value = int(self.lineEdit_value.text(), 16)
             index = self.btn.parent().parent().indexAt(self.btn.pos())
-            self.btn.parent().parent().cellWidget(index.row(), index.column()).setText(self.lineEdit_value.text())
             if self.btn.parent().parent() == self.Intel8080_MainWindow.Registers_table:
                 if 0 <= value < 256:  # May need to be different, depending on register
                     self.Intel8080_MainWindow.update_registers_table()
+                    self.btn.parent().parent().cellWidget(index.row(), index.column()).setText(
+                        self.lineEdit_value.text())
                     self.close()
             elif self.btn.parent().parent() == self.Intel8080_MainWindow.AdressLatch_table:
                 if 0 <= value < 2:  # May need to be different, depending on register
                     self.Intel8080_MainWindow.update_adressLatch_table()
+                    self.btn.parent().parent().cellWidget(index.row(), index.column()).setText(
+                        self.lineEdit_value.text())
                     self.close()
         except Exception:
             pass
